88063ac7dd4d93b6d96a60746041af3e1279b016
Andrew S. Brown <andrew@dontfidget.com>
2014-10-09 22:09:26 -0700

    remove debugging code

--- lib/chartnado/series/wrap.rb
+++ lib/chartnado/series/wrap.rb
@@ -115,7 +115,6 @@ module Chartnado
             hash
           end
         else
-          p 'here'
           with_precision(precision, to_f * multiplier.to_f / bottom.to_f)
         end
       end
